f59f2681a929df0585b1f5dac5dc579d072f0f72
Prithivi Pemi Magar <prithivipemimagar@gmail.com>
2025-12-20 22:44:41 +0545

    feat: add APIError class for structured error handling

---
+++ backend/src/lib/api-error.lib.js
@@ -0,0 +1,23 @@
+class APIError extends Error {
+  constructor(
+    statusCode = 500,
+    message = 'Internal Server Error',
+    error,
+    stack
+  ) {
+    super(message);
+    this.name = this.constructor.name;
+    this.statusCode = statusCode;
+    this.message = message;
+    this.success = false;
+    this.error = error;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export default APIError;

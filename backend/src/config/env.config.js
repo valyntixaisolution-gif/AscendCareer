2ddd3e37110638b4c3a8fba729c7ee6eb90623b9
Prithivi Pemi Magar <prithivipemimagar@gmail.com>
2025-12-20 22:44:53 +0545

    feat: add environment configuration with validation and logging

---
+++ backend/src/config/env.config.js
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import { validateEnv } from '../utils/index.util.js';
+import { envSchema } from '../validator/env.validator.js';
+
+const envConfig = {
+  PORT: process.env.PORT,
+  NODE_ENV: process.env.NODE_ENV,
+  LOG_DIR: process.env.LOG_DIR,
+  LOG_LEVEL: process.env.LOG_LEVEL,
+  JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET,
+  JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET,
+  JWT_ACCESS_TOKEN_EXPIRATION: process.env.JWT_ACCESS_TOKEN_EXPIRATION,
+  JWT_REFRESH_TOKEN_EXPIRATION: process.env.JWT_REFRESH_TOKEN_EXPIRATION,
+  DATABASE_URL: process.env.DATABASE_URL,
+};
+
+const config = validateEnv(envSchema, envConfig);
+
+console.log('Environment Config:', config);
+
+export default config;

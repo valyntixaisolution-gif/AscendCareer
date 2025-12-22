3f41175d9e05a60f77a1a454debb89d0db508d67
prithiviPM <prithivipemimagar@gmail.com>
2025-12-22 22:18:25 +0545

    feat: implement authentication middleware for token verification and role management

---
+++ backend/src/middlewares/authenticate.middleware.js
@@ -0,0 +1,61 @@
+import APIError from '../lib/api-error.lib.js';
+import jwtLib from '../lib/jwt.lib.js';
+import logger from '../lib/logger.lib.js';
+
+async function authenticateMiddleware(allowRoles = []) {
+  return async (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      logger.warn('No authorization header present', {
+        label: 'AuthenticateMiddleware',
+      });
+      return next(
+        new APIError(401, 'Unauthorized: No authorization header present')
+      );
+    }
+
+    const [schema, token] = authorization.split(' ');
+    if (schema !== 'Bearer' || !token) {
+      logger.warn('Invalid authorization format', {
+        label: 'AuthenticateMiddleware',
+      });
+      return next(
+        new APIError(401, 'Unauthorized: Invalid authorization format')
+      );
+    }
+
+    try {
+      const payload = jwtLib.verifyAccessToken(token);
+
+      if (!payload?.userId || !payload?.role) {
+        logger.warn('Invalid token payload', {
+          label: 'AuthenticateMiddleware',
+        });
+        return next(new APIError(401, 'Unauthorized: Invalid token payload'));
+      }
+
+      req.user = {
+        userId: payload.userId,
+        role: payload.role,
+      };
+
+      if (allowRoles.length > 0 && !allowRoles.includes(payload.role)) {
+        logger.warn(`User role ${payload.role} not authorized`, {
+          label: 'AuthenticateMiddleware',
+          userId: payload.userId,
+        });
+        return next(new APIError(403, 'Forbidden: User role not authorized'));
+      }
+
+      next();
+    } catch (error) {
+      logger.error('Error verifying access token', {
+        label: 'AuthenticateMiddleware',
+        error: error.message,
+      });
+      return next(new APIError(401, 'Unauthorized: Invalid access token'));
+    }
+  };
+}
+export default authenticateMiddleware;

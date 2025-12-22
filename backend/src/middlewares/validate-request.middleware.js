2e8fd3a4eddaac30be9d239af89298cd42d49040
prithiviPM <prithivipemimagar@gmail.com>
2025-12-22 11:18:28 +0545

    feat: add request validation middleware for improved error handling

---
+++ backend/src/middlewares/validate-request.middleware.js
@@ -0,0 +1,39 @@
+import APIError from '../lib/api-error.lib.js';
+import logger from '../lib/logger.lib.js';
+import { formatIssues } from '../utils/index.util.js';
+
+function validatePart(part, schema, req, next) {
+  if (!schema) return true;
+  const parsedPart = schema.safeParse(req[part]);
+
+  if (!parsedPart.success) {
+    const issues = formatIssues(parsedPart.error.issues);
+    logger.error(`${part} Validation Issues: `, {
+      type: 'ValidateRequestMiddleware',
+      issues,
+    });
+    next(new APIError('Validation Error', 400, issues));
+    return false;
+  }
+
+  req[part] = parsedPart.data;
+  return true;
+}
+
+const validateRequestMiddleware = (schema) => (req, res, next) => {
+  try {
+    if (!validatePart('body', schema?.body, req, next)) return;
+    if (!validatePart('query', schema?.query, req, next)) return;
+    if (!validatePart('params', schema?.params, req, next)) return;
+
+    next();
+  } catch (error) {
+    logger.error('ValidateRequestMiddleware Error: ', {
+      type: 'ValidateRequestMiddleware',
+      error,
+    });
+    next(new APIError('Internal Server Error', 500));
+  }
+};
+
+export default validateRequestMiddleware;

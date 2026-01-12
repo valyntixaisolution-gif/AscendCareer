b240f42c5bd9235400e192558a351c11bb900e39
prithiviPM <prithivipemimagar@gmail.com>
2026-01-12 16:37:21 +0545

    feat: implement assignment routes with middleware for API rate limiting and authentication

--- backend/src/routes/assignment.route.js
+++ backend/src/routes/assignment.route.js
@@ -1,15 +1,62 @@
 import { Router } from 'express';
+import { apiRateLimiter } from '../middlewares/rate-limiting.middleware';
+import authenticateMiddleware from '../middlewares/authenticate.middleware';
+import validateRequestMiddleware from '../middlewares/validate-request.middleware';
+import {
+  createAssignmentSchema,
+  updateAssignmentSchema,
+} from '../validator/assignment.validator';
+import asyncHandlerMiddleware from '../middlewares/async-handler.middleware';
+import {
+  createAssignment,
+  getAssignmentsByCourse,
+  getAssignmentById,
+  updateAssignment,
+  deleteAssignment,
+} from '../controllers/assignment.controller.js';
 
 const router = Router();
 
-router.route('/courses/:courseId/assignments').post();
+router
+  .route('/courses/:courseId/assignments')
+  .post(
+    apiRateLimiter,
+    authenticateMiddleware(),
+    validateRequestMiddleware(createAssignmentSchema),
+    asyncHandlerMiddleware(createAssignment)
+  );
 
-router.route('/courses/:courseId/assignments').get();
+router
+  .route('/courses/:courseId/assignments')
+  .get(
+    apiRateLimiter,
+    authenticateMiddleware(),
+    asyncHandlerMiddleware(getAssignmentsByCourse)
+  );
 
-router.route('/assignments/:assignmentId').get();
+router
+  .route('/assignments/:assignmentId')
+  .get(
+    apiRateLimiter,
+    authenticateMiddleware(),
+    asyncHandlerMiddleware(getAssignmentById)
+  );
 
-router.route('/assignments/:assignmentId').put();
+router
+  .route('/assignments/:assignmentId')
+  .put(
+    apiRateLimiter,
+    authenticateMiddleware(),
+    validateRequestMiddleware(updateAssignmentSchema),
+    asyncHandlerMiddleware(updateAssignment)
+  );
 
-router.route('/assignments/:assignmentId').delete();
+router
+  .route('/assignments/:assignmentId')
+  .delete(
+    apiRateLimiter,
+    authenticateMiddleware(),
+    asyncHandlerMiddleware(deleteAssignment)
+  );
 
 export default router;
